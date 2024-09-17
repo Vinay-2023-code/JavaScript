8d5358b63d3e7b8d42e3444aea23c25544d6b32f
Vinay Rajbhar <vinayrajbhar2023@gmail.com>
2024-09-17 19:32:44 +0530

    Added Changes

---
+++ variable.js
@@ -0,0 +1,25 @@
+// Global scope
+// var a = 5;
+// console.log(a);
+
+
+// Block Scope
+// {
+//     var a =5;
+// }
+// console.log(a);
+// var will be executed in block scope also
+// whereas it will not be executed with let
+// {
+//     let a = 5;
+// }
+// console.log(a);
+// function test() {
+//     let a = "hii";
+//     if (true) {
+//         let a = "hello";
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// test();
