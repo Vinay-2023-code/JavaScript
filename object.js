5650234ce2f9a183c4809b5a3f766dc0f9dd87a1
Vinay Rajbhar <vinayrajbhar2023@gmail.com>
2024-09-18 16:17:45 +0530

    Added changes

--- object.js
+++ object.js
@@ -10,9 +10,43 @@
 // array[2]= 8;
 // console.log (array);
 
-let myName ={firstName: "Vipin", lastName:"Kumar"};
-function fullName(a) { 
-    return a;
-}
-myName.lastName = "Singh";
-console.log(fullName(myName));
+// let myName ={firstName: "Vipin", lastName:"Kumar"};
+// function fullName(a) { 
+//     return a;
+// }
+// myName.lastName = "Singh";
+// console.log(fullName(myName));
+
+
+let a =[{company:"hp", model: "g20", }, {company:"dell", model:"inspiron-21"}, {company:"hp", model:"mac m2"}, {company:"lenovo", model:"lv23"}]
+let b =[{car:"maruti"}];
+// let result = array.map (function(a){
+//     // console.log(a);
+//     // a.company = "256gb";
+//     if(a.company=="hp"){
+//         a.company ="apple";
+//     }else{
+//         a.company = "dell";
+//     }
+//     return a; 
+// })
+// console.log(result);
+
+// for (let index = 0; index < a.length; index++) {
+//     let element = a[index];
+//     console.log(element.company);
+//     if(element.company != "hp"){
+//         // break;
+//     }
+// }
+// a.forEach( function(d) {
+//     console.log(d);
+// });
+// let result = a.slice(1,3);
+// console.log(result);
+// console.log(a);
+
+
+// let result = a.concat(b);
+// console.log(result);
+// console.log(a);
