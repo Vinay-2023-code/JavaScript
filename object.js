eb9c2ed63e2507af4797e30b41f380bf1ee14593
Vinay Rajbhar <vinayrajbhar2023@gmail.com>
2024-09-19 12:46:50 +0530

    Added changes

--- object.js
+++ object.js
@@ -18,8 +18,8 @@
 // console.log(fullName(myName));
 
 
-let a =[{company:"hp", model: "g20", }, {company:"dell", model:"inspiron-21"}, {company:"hp", model:"mac m2"}, {company:"lenovo", model:"lv23"}]
-let b =[{car:"maruti"}];
+// let a =[{company:"hp", model: "g20", }, {company:"dell", model:"inspiron-21"}, {company:"hp", model:"mac m2"}, {company:"lenovo", model:"lv23"}]
+// let b =[{car:"maruti"}];
 // let result = array.map (function(a){
 //     // console.log(a);
 //     // a.company = "256gb";
@@ -50,3 +50,40 @@ let b =[{car:"maruti"}];
 // let result = a.concat(b);
 // console.log(result);
 // console.log(a);
+
+
+
+// function add(a,b){
+//     return a+b;
+// }
+// function sub(a,b) {
+//     return a-b;
+// }
+// function total(a){
+//     return a;
+// }
+// add(5,5);
+// sub(50,30);
+// let addition = add(5,5);
+// let subtraction = sub(50,30);
+// console.log(total(addition + subtraction));
+
+
+// function myName(fName,lName){
+//     return fName +" "+ lName;
+// }
+// console.log(myName("Vinay","Rajbhar"));
+
+// let x = 10;
+// let y = 20;
+// function add(a,b) {
+//     return a+b;
+// }
+// console.log(add(x,y));
+
+
+
+let car = {company : "dell", model : "insp1521", colour: "black",}
+console.log(car.company);
+console.log(car.model);
+
